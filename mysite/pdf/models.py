8b74a3eaefbf7d8295483d7954d145dc01bd20dc
alijamil22 <alijamil2223344@gmail.com>
2026-01-21 19:01:37 +0500

    Improve views.py to get data from forms and store it in database.

--- mysite/pdf/models.py
+++ mysite/pdf/models.py
@@ -11,3 +11,5 @@ class Profile(models.Model):
     summary = models.TextField(max_length=2000)
     previous_work = models.TextField(max_length=1000)
     skills = models.TextField(max_length=1000)
+    def __str__(self):
+        return "Profile"
--- mysite/pdf/views.py
+++ mysite/pdf/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from .models import Profile
 # Create your views here.
 def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        email = request.POST.get("email")
+        degree = request.POST.get("degree")
+        school = request.POST.get("school")
+        university = request.POST.get("university")
+        summary = request.POST.get("summary")
+        skills = request.POST.get("skills")
+        previous_work = request.POST.get("previous_work")
+        profile = Profile(name=name,email=email,phone=phone,degree=degree,school=school,university=university,summary=summary,skills=skills,previous_work=previous_work)
+        profile.save()
     return render(request,'pdf/accept.html')
